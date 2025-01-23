c213f8b3508f65bb585c46a191a6ef63a02127d2
Emily Matheys <emilymatt96@gmail.com>
2025-01-23 22:17:15 +0200

    initial commit

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
