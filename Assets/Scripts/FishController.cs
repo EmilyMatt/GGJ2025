3f546ede1c9775c1ba53979c5ee723962965680c
Emily Matheys <emilymatt96@gmail.com>
2025-01-23 22:42:40 +0200

    canvas camera overlay

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class FishController : MonoBehaviour
 {
+    public Vector4 aquariumRanges;
+    private float _delay;
     private Rigidbody2D _rigidbody;
+
+    private Vector3? _targetPoint;
 
     // Start is called before the first frame update
     private void Start()
     {
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (_targetPoint.HasValue)
+        {
+            var distanceToTarget = Vector3.Distance(transform.position, _targetPoint.Value);
+            if (Vector3.Distance(transform.position, _targetPoint.Value) < 0.1f)
+            {
+                _targetPoint = null;
+                _delay -= Time.deltaTime;
+            }
+            else
+            {
+                _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, _targetPoint.Value, Time.deltaTime));
+            }
+        }
+        else
+        {
+            _targetPoint = new Vector3(Random.Range(aquariumRanges.x, aquariumRanges.y),
+                Random.Range(aquariumRanges.z, aquariumRanges.w), 0);
+            _delay = 2;
+        }
     }
 }
