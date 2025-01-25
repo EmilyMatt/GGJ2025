486b065d01373c6e5b670d514778068e4221a407
Emily Matheys <emilymatt96@gmail.com>
2025-01-25 18:18:57 +0200

    Fix fish sizes, less common poison plant

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Shop : MonoBehaviour
 {
     public float margin = 10.0F;
     public float generationTime;
     public GameObject shopItem;
+    private float _internalGenerationTime;
 
     private float _lastShopItemGenerated;
 
-    // Update is called once per frame
-    private void Update()
+
+    private void Start()
     {
-        if (!Camera.main || !(_lastShopItemGenerated + generationTime < Time.fixedTime)) return;
+        _internalGenerationTime = Random.Range(generationTime * 0.9f, generationTime * 1.1f);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!Camera.main || !(_lastShopItemGenerated + _internalGenerationTime < Time.fixedTime)) return;
         _lastShopItemGenerated = Time.fixedTime;
+        _internalGenerationTime = Random.Range(generationTime * 0.9f, generationTime * 1.1f);
 
         var pos = new Vector3(Random.Range(margin, Screen.width - margin), 20.0F, 10.0F);
         var v3 = Camera.main.ScreenToWorldPoint(pos);
@@ -23,7 +32,7 @@ public class Shop : MonoBehaviour
         {
             < 1 => ShopItem.ItemType.PurpleFish,
             < 4 => ShopItem.ItemType.Goldfish,
-            < 7 => ShopItem.ItemType.Poisionplant,
+            < 6 => ShopItem.ItemType.Poisionplant,
             _ => ShopItem.ItemType.Plant
         };
     }
